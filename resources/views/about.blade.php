c97b990de1fbe4558b1fd50d7e38d3c45c082057
Alex Yap <registrar@lexethos.com>
2018-01-26 15:35:17 +0800

    Lesson 2 :  Basic Routing and Views
    Your first step, when learning Laravel, should be to visit your main routes/routes.php file. Here, we may define any number of routes for our application. Or, in other words, when the user visits a particular URL, what logic should we trigger?

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About LaraPlay</title>
+</head>
+<body>
+	This is the Laravel Playground.
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaraPlay
                 </div>
 
                 <div class="links">
